09e08495d4981799dd2e22592601ae4c1f7e04f1
antonioporpeta1 <antonio.porpeta1@gmail.com>
2019-09-23 20:27:21 -0300

    Atualizando Categoria com PUT, outros pequenos ajustes

--- src/main/java/com/nelioalves/cursomc/services/CategoriaService.java
+++ src/main/java/com/nelioalves/cursomc/services/CategoriaService.java
@@ -15,7 +15,7 @@ public class CategoriaService {
 	@Autowired
 	private CategoriaRepository repo;
 	
-	public Categoria buscar(Integer id) {
+	public Categoria find(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
 		
 		if (!obj.isPresent()) {
@@ -25,8 +25,13 @@ public class CategoriaService {
 		return obj.orElse(null);
 	}
 
-	public Categoria Insert(Categoria obj) {
+	public Categoria insert(Categoria obj) {
 		obj.setId(null);
+		return repo.save(obj);
+	}
+	
+	public Categoria update(Categoria obj) {
+		find(obj.getId());
 		return repo.save(obj);
 	}
 }
